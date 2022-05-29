fe2f03f469dd6e9c4e9f1a974771c516732d56ba
Sam <svyatoslav19cm@mail.ru>
2022-05-29 16:27:55 +0300

    init

---
+++ setup.py
@@ -0,0 +1,21 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import pathlib
+
+from setuptools import find_packages, setup
+
+here = pathlib.Path(__file__).parent.resolve()
+
+install_requires = (here / 'requirements.txt').read_text(encoding='utf-8').splitlines()
+
+setup(
+    install_requires=install_requires,
+    package_dir={'': 'lib'},
+    packages=find_packages('lib'),
+    entry_points={
+        'console_scripts': [
+            'tracker-start=tracker.cli.adhoc:main',
+        ],
+    },
+)
---
+++ tracker/__init__.py
@@ -0,0 +1,2 @@
+__app_name__ = 'tracker'
+__version__ = '0.1.0'
