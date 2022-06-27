f7ab0821ca72bd9ffc7cdf20d9930b768ada7eda
Sam <svyatoslav19cm@mail.ru>
2022-06-28 02:24:55 +0300

    fix: database models init file

--- lib/tracker/__init__.py
+++ lib/tracker/__init__.py
@@ -1,7 +1,7 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
 from tracker.release import __version__, __author__, __app_name__
 from cli.console import app
 
-def main():
-    app(prog_name=__app_name__)
-
-main()
+app(prog_name=__app_name__)
